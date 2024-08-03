efa855317b1192d0418a09d8a4b72f4adc1a6c9e
SangWookie <swcube520@gmail.com>
2024-08-03 18:10:04 +0900

    Initial commit

---
+++ src/test/java/kpaas/cumulonimbus/servicediscoveryserver/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kpaas.cumulonimbus.servicediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
